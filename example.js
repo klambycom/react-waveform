64b18e3cb50baeefd5a943ac6f70cf87a3b458ab
klambycom <christian@klamby.com>
2015-02-14 17:51:42 +0100

    Better example

--- example.js
+++ example.js
@@ -1,7 +1,22 @@
 var React = require('react');
 var Waveform = require('./index');
 
-React.render(
-    <Waveform />,
-    document.getElementById('waveform')
-    );
+var request = new XMLHttpRequest();
+request.open('GET', 'HardaTider-Har&nu.mp3', true);
+request.responseType = 'arraybuffer';
+
+request.addEventListener('load', function () {
+  var context = new (window.AudioContext || window.webkitAudioContext)();
+
+  context.decodeAudioData(request.response, function (buffer) {
+    var source = context.createBufferSource();
+    source.buffer = buffer;
+
+    React.render(
+        <Waveform buffer={buffer} />,
+        document.getElementById('waveform')
+        );
+  });
+});
+
+request.send();
